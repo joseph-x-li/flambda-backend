1f09f975c1a265b82809c988deca31efec1fcc0e
Greta Yorsh <gyorsh@janestreet.com>
2021-04-29 11:04:38 +0100

    Add Cprefetch to Proc.operation_supported

--- backend/amd64/proc.ml
+++ backend/amd64/proc.ml
@@ -404,6 +404,7 @@ let init () =
 let operation_supported = function
   (* XCR mshinwell: This should be an exhaustive match. *)
   | Cpopcnt -> !popcnt_support
+  | Cprefetch _
   | Capply _ | Cextcall _ | Cload _ | Calloc | Cstore _
   | Caddi | Csubi | Cmuli | Cmulhi | Cdivi | Cmodi
   | Cand | Cor | Cxor | Clsl | Clsr | Casr
