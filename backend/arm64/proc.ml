58169da758ea8c4d25f42b2863ce10a7acb82e15
Greta Yorsh <gyorsh@janestreet.com>
2022-01-19 09:51:53 +0000

    Add Cbswap

--- backend/arm64/proc.ml
+++ backend/arm64/proc.ml
@@ -314,6 +314,7 @@ let operation_supported = function
   | Cclz _ | Cctz _ | Cpopcnt
   | Cprefetch _
     -> false   (* Not implemented *)
+  | Cbswap _
   | Capply _ | Cextcall _ | Cload _ | Calloc | Cstore _
   | Caddi | Csubi | Cmuli | Cmulhi _ | Cdivi | Cmodi
   | Cand | Cor | Cxor | Clsl | Clsr | Casr
